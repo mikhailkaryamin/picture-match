8d7543685b6aca19b8c63df2a95d3da60d618c5a
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-09-26 14:33:36 +0500

    feat(Menu): create new component

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <div className="menu">
+      <ul className="menu__list">
+        <li className="menu__item">
+          New game
+        </li>
+        <li className="menu__item">
+          Restart
+        </li>
+        <li className="menu__item">
+          High score
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Menu;

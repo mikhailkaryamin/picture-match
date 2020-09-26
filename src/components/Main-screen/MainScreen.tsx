4dec5da6eab61a96ca26d4d5ec040f167f265839
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-09-26 14:33:24 +0500

    feat(MainScreen): create new component

---
+++ src/components/Main-screen/MainScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Menu from '../Menu/Menu'
+
+const MainScreen = () => {
+  return (
+    <div className="main-screen">
+      <div className="main-screen__wrapper">
+        <Menu />
+      </div>
+    </div>
+  )
+}
+
+export default MainScreen;

58e4ac13a2fcadba526ef365b0fce38f5a66b469
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-09-27 20:48:31 +0500

    feat(Frame): create component. WIP

---
+++ src/components/Frame/Frame.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import Card from '../Card/Card';
+
+type Props = {
+  cards: {
+    imgSrc: string,
+    title: string,
+  }[];
+  sizeFrame: number[];
+}
+
+const Frame: React.FC<Props> = ({ cards, sizeFrame}: Props) => {
+  return (
+    <div className="frame">
+      <ul className="frame__cards-list">
+        {cards.map((card) => {
+          const {
+            imgSrc,
+            title
+          } = card;
+
+          return (
+            <li className={`frame__card frame__card--${sizeFrame[0]}x${sizeFrame[1]}`}>
+              <Card
+                img={imgSrc}
+                title={title}
+              />
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default Frame;

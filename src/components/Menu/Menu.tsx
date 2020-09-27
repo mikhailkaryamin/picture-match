029474d8d7b1e5d8d5218b03786be6fe718ec7f6
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-09-27 20:50:22 +0500

    feat(Menu): add Link to New game, High score

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { AppRoute } from '../../shared/routes';
 
 const Menu = () => {
   return (
     <div className="menu">
       <ul className="menu__list">
         <li className="menu__item">
-          New game
+          <Link
+            className="menu__link"
+            to={AppRoute.GAME}
+          >
+            New game
+          </Link>
         </li>
         <li className="menu__item">
           Restart
         </li>
         <li className="menu__item">
-          High score
+          <Link
+            className="menu__link"
+            to={AppRoute.HIGH_SCORE}
+          >
+            High score
+          </Link>
         </li>
       </ul>
     </div>

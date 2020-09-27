1bbdd0902c953b123ded9b50dea4be63b5da17aa
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-09-27 20:49:22 +0500

    feat(HighScoreScreen): create component

---
+++ src/components/High-Score-Screen/HighScoreScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Container from '../Container/Container';
+
+const HighScoreScreen = () => {
+  return (
+    <Container>
+      <div className="high-score-screen">
+
+      </div>
+    </Container>
+  )
+}
+
+export default HighScoreScreen;

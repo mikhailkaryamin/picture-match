28ebc28886639287aaa4038d5d8fea585b06833c
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-09-27 20:46:29 +0500

    feat(App): add Router, MainScreen, GameScreen, HighScoreScreen

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
-import GameContainer from '../Game-container/GameContainer';
-import MainScreen from '../Main-screen/MainScreen';
+import { AppRoute } from '../../shared/routes'
 
+import MainScreen from '../Main-Screen/MainScreen';
+import GameScreen from '../Game-Screen/GameScreen';
+import HighScoreScreen from '../High-Score-Screen/HighScoreScreen';
 
 function App () {
   return(
-    <GameContainer>
-      <MainScreen />
-    </GameContainer>
+    <Router>
+      <Switch>
+        <Route exact path={AppRoute.MAIN}>
+          <MainScreen />
+        </Route>
+        <Route path={AppRoute.GAME}>
+          <GameScreen />
+        </Route>
+        <Route path={AppRoute.HIGH_SCORE}>
+          <HighScoreScreen />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 

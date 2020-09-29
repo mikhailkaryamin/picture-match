ac26bb1a71f13ba1ccb37e6dde63f097dc696195
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-09-29 15:59:50 +0500

    chore(icon-animals): add images

---
+++ src/assets/animals/index.ts
@@ -0,0 +1,84 @@
+import back from './back.svg';
+import bear from './bear.svg';
+import bee from './bee.svg';
+import chicken from './chicken.svg';
+import dog from './dog.svg';
+import elephant from './elephant.svg';
+import flamingo from './flamingo.svg';
+import frog from './frog.svg';
+import giraffe from './giraffe.svg';
+import horse from './horse.svg';
+import koala from './koala.svg';
+import lion from './lion.svg';
+import owl from './owl.svg';
+import pig from './pig.svg';
+import spider from './spider.svg';
+import turtle from './turtle.svg';
+
+const Animals = {
+  BACK: {
+    srcImg: back,
+    title: 'Back',
+  },
+  BEAR: {
+    srcImg: bear,
+    title: 'Bear',
+  },
+  BEE: {
+    srcImg: bee,
+    title: 'Bee',
+  },
+  CHICKEN: {
+    srcImg: chicken,
+    title: 'Chicken',
+  },
+  DOG: {
+    srcImg: dog,
+    title: 'Dog',
+  },
+  ELEPHANT: {
+    srcImg: elephant,
+    title: 'Elephant',
+  },
+  FLAMINGO: {
+    srcImg: flamingo,
+    title: 'Flamingo',
+  },
+  FROG: {
+    srcImg: frog,
+    title: 'Frog',
+  },
+  GIRAFFE: {
+    srcImg: giraffe,
+    title: 'Giraffe',
+  },
+  HORSE: {
+    srcImg: horse,
+    title: 'Horse',
+  },
+  KOALA: {
+    srcImg: koala,
+    title: 'Koala',
+  },
+  LION: {
+    srcImg: lion,
+    title: 'Lion',
+  },
+  OWL: {
+    srcImg: owl,
+    title: 'Owl',
+  },
+  PIG: {
+    srcImg: pig,
+    title: 'Pig',
+  },
+  SPIDER: {
+    srcImg: spider,
+    title: 'Spider',
+  },
+  TURTLE: {
+    srcImg: turtle,
+    title: 'Turtle',
+  }};
+
+export default Animals;

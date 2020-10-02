437437ed1bf7e5e4c4cf212436e5f1f55413ac9e
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-10-02 21:38:23 +0300

    feat(rootReducer): add empty reducer

---
+++ src/reducer/rootReducer.ts
@@ -0,0 +1,17 @@
+type Action = {
+  type: string;
+}
+
+const initialState = {};
+
+const reducer = (state = initialState, action: Action) => {
+  switch (action.type) {
+
+  }
+
+  return state;
+}
+
+export {
+  reducer,
+}

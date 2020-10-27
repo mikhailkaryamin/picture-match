6ff63da8f91130f0b4846ef56a822f590ae5d910
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-10-27 23:02:50 +0500

    feat(Button): add disabled attribute for button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 
 type Props = {
   children?: React.ReactNode;
+  isDisabled?: boolean;
   onClick: () => void;
   title: string;
   prefix: string;
 }
 
-const Button: React.FC<Props>  = ({children, onClick, prefix, title }: Props) => {
+const Button: React.FC<Props>  = ({children, isDisabled, onClick, prefix, title }: Props) => {
   return (
     <button
       className={`button button--${prefix}`}
       onClick={onClick}
+      disabled={isDisabled ? true : false}
     >
       <span className={`button__title button__title--${prefix}`}>
         {children === undefined && (

661bffe6312055da5df018a2da94919ccad6e252
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-11-03 23:11:59 +0500

    feat(Frame): add EndGame component

--- src/containers/Frame/Frame.tsx
+++ src/containers/Frame/Frame.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import CardFlip from '../CardFlip/CardFlip';
+import EndGame from '../../components/End-Game/End-Game';
 
 import {
   Card as CardType,
@@ -14,26 +15,36 @@ type Props = {
 }
 
 const Frame: React.FC<Props> = ({ cards, sizeFrame }: Props) => {
+
+  const isEndGame = cards.every((card) => !card.isVisible);
+
   return (
     <div className="frame">
-      <ul className="frame__cards-list">
-        {cards.map((card, i) => {
-
-          return (
-            <li
-              className={`frame__card frame__card--${sizeFrame[0]}x${sizeFrame[1]}`}
-              key={`${card}-${i}`}
-            >
-              <div className="frame__card-wrapper">
-                <CardFlip
-                  key={`${card.title}-${i}`}
-                  card={card}
-                />
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+      {isEndGame || (
+        <ul className="frame__cards-list">
+          {cards.map((card, i) => {
+            return (
+              <li
+                className={`frame__card frame__card--${sizeFrame[0]}x${sizeFrame[1]}`}
+                key={`${card}-${i}`}
+              >
+                <div className="frame__card-wrapper">
+                  <CardFlip
+                    key={`${card.title}-${i}`}
+                    card={card}
+                  />
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+      {isEndGame && (
+        <EndGame
+          currentTime={`00.10`}
+          bestTime={`00.10`}
+        />
+      )}
     </div>
   );
 };

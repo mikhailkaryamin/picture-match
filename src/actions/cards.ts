65a6c85c462d7dc15c444c0bfd42203a052de24d
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-11-03 23:04:31 +0500

    feat(action\cards): add checkMatchCards, resetCards, resetOpenCards, setFlipCard

--- src/actions/cards.ts
+++ src/actions/cards.ts
@@ -1,37 +1,55 @@
 import {
-  ActionFirstOpenCard,
-  ActionFrameSize,
-  ActionSetNumberOfCard,
-  ActionSecondOpenCard,
+  ActionsCards,
   Card as CardType,
 } from '../shared/types';
 
 const ActionType = {
+  CHECK_MATCH_CARDS: 'CHECK_MATCH_CARDS',
   SET_FIRST_OPEN_CARD: 'SET_FIRST_OPEN_CARD',
+  SET_FLIP_CARD: 'SET_FLIP_CARD',
   SET_FRAME_SIZE: 'SET_FRAME_SIZE',
   SET_NUMBER_OF_CARD: 'SET_NUMBER_OF_CARD',
   SET_SECOND_OPEN_CARD: 'SET_SECOND_OPEN_CARD',
+  RESET_CARDS: 'RESET_CARDS',
+  RESET_OPEN_CARDS: 'RESET_OPEN_CARDS',
 } as const;
 
 const ActionCreator = {
-  setFirstOpenCard: (card: CardType): ActionFirstOpenCard => ({
+  checkMatchCards: (): ActionsCards => ({
+    type: ActionType.CHECK_MATCH_CARDS,
+  }),
+
+  setFirstOpenCard: (card: CardType): ActionsCards => ({
     type: ActionType.SET_FIRST_OPEN_CARD,
     firstOpenCard: card,
   }),
 
-  setFrameSize: (size: number[]): ActionFrameSize => ({
+  setFlipCard: (id: number): ActionsCards => ({
+    type: ActionType.SET_FLIP_CARD,
+    id,
+  }),
+
+  setFrameSize: (size: number[]): ActionsCards => ({
     type: ActionType.SET_FRAME_SIZE,
     sizeFrame: size,
   }),
 
-  setNumberOfCards: (number: number): ActionSetNumberOfCard => ({
+  setNumberOfCards: (number: number): ActionsCards => ({
     type: ActionType.SET_NUMBER_OF_CARD,
     numberOfCards: number,
   }),
 
-  setSecondOpenCard: (card: CardType): ActionSecondOpenCard => ({
+  setSecondOpenCard: (card: CardType): ActionsCards => ({
     type: ActionType.SET_SECOND_OPEN_CARD,
     secondOpenCard: card,
+  }),
+
+  resetCards: (): ActionsCards => ({
+    type: ActionType.RESET_CARDS,
+  }),
+
+  resetOpenCards: (): ActionsCards => ({
+    type: ActionType.RESET_OPEN_CARDS,
   }),
 };
 

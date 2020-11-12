a0e52f270990a80c5e3ef0675d872635c17f9edb
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-11-12 22:17:35 +0500

    feat(PlayButton): useSelector, useDispatch, shallowEqual instead connect

--- src/containers/PlayButton/PlayButton.tsx
+++ src/containers/PlayButton/PlayButton.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {
+  useDispatch,
+  useSelector,
+} from 'react-redux';
 
 import Button from '../../components/Button/Button';
 import { ReactComponent as Play } from '../../assets/icons/play.svg';
@@ -7,20 +10,16 @@ import { ReactComponent as Pause } from '../../assets/icons/pause.svg';
 
 import { ActionCreator as ActionTimer } from '../../actions/timer';
 
-import {
-  ActionsTimer,
-  State as StateType
-} from '../../shared/types';
+import { State as StateType } from '../../shared/types';
 
-type Dispatch = (arg: ActionsTimer) => void;
 
-type Props = {
-  isPlay: boolean;
-  start: () => void;
-  stop: () => void;
-}
+const PlayButton: React.FC = () => {
+  const dispatch = useDispatch();
 
-const PlayButton: React.FC<Props> = ({ isPlay, start, stop }: Props) => {
+  const isPlay = useSelector((state: StateType) => state['TIMER'].isActive);
+  const start = () => dispatch(ActionTimer.setActive(true));
+  const stop = () => dispatch(ActionTimer.setActive(false));
+
   return (
     <Button
       onClick={isPlay ? stop : start}
@@ -32,24 +31,4 @@ const PlayButton: React.FC<Props> = ({ isPlay, start, stop }: Props) => {
   );
 };
 
-const mapStateToProps = (state: StateType) => ({
-  isPlay: state['TIMER'].isActive,
-});
-
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-  start: () => {
-    dispatch(ActionTimer.setActive(
-        true
-    ));
-  },
-  stop: () => {
-    dispatch(ActionTimer.setActive(
-        false
-    ));
-  }
-});
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(PlayButton);
+export default PlayButton;

0c4ae940d799ea7934d10b191c879da4e7dad90f
mikhailkaryamin <karyamin.mihail@yandex.ru>
2021-01-13 14:09:21 +0300

    fix(Main-Screen): fix name folder

---
+++ src/components/Main-Screen/MainScreen.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Container from '../Container/Container';
+import Menu from '../Menu/Menu';
+
+const MainScreen: React.FC = () => {
+  return (
+    <Container>
+      <div className="main-screen">
+        <div className="main-screen__wrapper">
+          <Menu />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default MainScreen;

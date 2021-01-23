7167e85d201bb945a5cf00fc358885d41a795cfe
mikhailkaryamin <karyamin.mihail@yandex.ru>
2021-01-23 16:32:26 +0300

    chore(App): add base name in Router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { AppRoute } from '../../shared/routes';
 
@@ -11,19 +7,15 @@ import GameScreen from '../Game-Screen/GameScreen';
 import HighScoreScreen from '../High-Score-Screen/HighScoreScreen';
 import MainScreen from '../Main-Screen/MainScreen';
 
+const BASE_NAME = '/picture-match';
+
 const App: React.FC = () => {
   return (
-    <Router>
+    <Router basename={BASE_NAME}>
       <Switch>
-        <Route exact path={AppRoute.MAIN}>
-          <MainScreen />
-        </Route>
-        <Route path={AppRoute.GAME}>
-          <GameScreen />
-        </Route>
-        <Route path={AppRoute.HIGH_SCORE}>
-          <HighScoreScreen />
-        </Route>
+        <Route exact path={AppRoute.MAIN} render={() => <MainScreen />} />
+        <Route exact path={AppRoute.GAME} render={() => <GameScreen />} />
+        <Route exact path={AppRoute.HIGH_SCORE} render={() => <HighScoreScreen />} />
       </Switch>
     </Router>
   );
